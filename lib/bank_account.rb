de462f85db08fe0f5538ef2dae8653d2de799af9
Catherine Bronzo <cbronzo@mac.com>
2020-12-14 02:20:00 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -7,5 +7,21 @@ class BankAccount
     @balance = 1000
     @status = "open"
   end
+  
+   def deposit(deposit_amt)
+    self.balance += deposit_amt
+  end
+
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+
+  def valid?
+    balance > 0 && status == "open"
+  end
 
 end
